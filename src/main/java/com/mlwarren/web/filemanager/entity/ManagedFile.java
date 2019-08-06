23b6247ae004e2360cdb87a3c862cfbbb02b472a
Mike Warren <mlwarren88@gmail.com>
2019-08-05 22:40:44 -0400

    update lombok annotations in ManagedFile so file id will be returned in json object

--- src/main/java/com/mlwarren/web/filemanager/entity/ManagedFile.java
+++ src/main/java/com/mlwarren/web/filemanager/entity/ManagedFile.java
@@ -4,25 +4,36 @@ package com.mlwarren.web.filemanager.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
-@Data
 public class ManagedFile {
     @Id
+    @Getter
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+    @Getter
+    @Setter
     private String fileName;
 
+    @Getter
+    @Setter
     private long fileSize;
 
+    @Getter
+    @Setter
     private String fileType;
 
+    @Getter
+    @Setter
     private Integer downloadCount;
 
     @ManyToOne(fetch= FetchType.LAZY)
     @JsonIgnore
+    @Getter
+    @Setter
     private User user;
 }
